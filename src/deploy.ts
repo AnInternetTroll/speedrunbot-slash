d3c83e736cea415a76c4a846ddddfb61139a11cd
Luca Matei Pintilie <lucafulger@gmail.com>
2021-09-16 20:48:13 +0200

    Forgot to remove this one line

--- src/deploy.ts
+++ src/deploy.ts
@@ -3,14 +3,14 @@
 /// <reference lib="deno.ns" />
 /// <reference lib="deno.unstable" />
 
-import { client, init } from "./deps_harmony.ts";
+import { client } from "./deps_harmony.ts";
 import { SpeedrunCom } from "./srcom/slashCommands.ts";
 
 // Disabling the frontend
 // How do I use this with harmony???
 import { start } from "https://raw.githubusercontent.com/lucacasonato/fresh/main/server.ts";
 import routes from "./routes.gen.ts";
+
 start(routes);
 
-init({ env: true, path: "/discord/interactions" });
 client.loadModule(new SpeedrunCom());
