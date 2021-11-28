dea871803fdd76973cd295aa07dfd7aa780b970d
Luca Matei Pintilie <lucafulger@gmail.com>
2021-11-28 11:01:19 +0100

    Bigger autocomplete test

--- src/pages/api/discord/interactions.ts
+++ src/pages/api/discord/interactions.ts
@@ -1,16 +1,4 @@
-// deno-lint-ignore-file no-explicit-any
-import {
-	ApplicationCommandInteraction,
-	decodeText,
-	HandlerContext,
-	Interaction,
-	InteractionApplicationCommandResolved,
-	InteractionPayload,
-	InteractionResponseType,
-	InteractionsClient,
-	InteractionType,
-	User,
-} from "../../../deps_server.ts";
+import { HandlerContext, InteractionsClient } from "../../../deps_server.ts";
 import { SpeedrunCom } from "../../../srcom/slash_commands.ts";
 
 export const client = new InteractionsClient({
@@ -20,91 +8,13 @@ export const client = new InteractionsClient({
 
 client.loadModule(new SpeedrunCom());
 
-const bad = new Response(
-	JSON.stringify({ code: 400, message: "Bad request " }),
-	{
-		status: 400,
-	},
-);
 export const handler = {
-	async POST(ctx: HandlerContext): Promise<Response> {
-		const req = ctx.req;
-		try {
-			if (req.bodyUsed === true) throw new Error("Request Body already used");
-			if (req.body === null) return bad;
-			const rawbody = (await req.body.getReader().read()).value;
-			if (rawbody === undefined) return bad;
-
-			if (req.method.toLowerCase() !== "post") return bad;
-
-			const signature = req.headers.get("x-signature-ed25519");
-			const timestamp = req.headers.get("x-signature-timestamp");
-			if (signature === null || timestamp === null) return bad;
-
-			const verify = await client.verifyKey(rawbody, signature, timestamp);
-			if (!verify) return bad;
-
-			let res: ApplicationCommandInteraction | Interaction;
-			try {
-				const payload: InteractionPayload = JSON.parse(decodeText(rawbody));
-				if (payload.type === InteractionType.APPLICATION_COMMAND) {
-					res = new ApplicationCommandInteraction(client as any, payload, {
-						user: new User(
-							client as any,
-							(payload.member?.user ?? payload.user)!,
-						),
-						member: payload.member as any,
-						guild: payload.guild_id as any,
-						channel: payload.channel_id as any,
-						resolved: ((payload.data as any)
-							?.resolved as unknown as InteractionApplicationCommandResolved) ??
-							{
-								users: {},
-								members: {},
-								roles: {},
-								channels: {},
-							},
-					});
-				} else {
-					res = new Interaction(client as any, payload, {
-						user: new User(
-							client as any,
-							(payload.member?.user ?? payload.user)!,
-						),
-						member: payload.member as any,
-						guild: payload.guild_id as any,
-						channel: payload.channel_id as any,
-					});
-				}
-
-				if (res.type === InteractionType.PING) {
-					await res.respond({ type: InteractionResponseType.PONG });
-					client.emit("ping");
-					return bad;
-				}
-
-				await client.emit("interaction", res);
-				await (client as any)._process(res);
-
-				return new Response(
-					res instanceof FormData ? res : JSON.stringify(res),
-					{
-						status: 200,
-						headers: new Headers({
-							"content-type": res instanceof FormData
-								? "multipart/form-data"
-								: "application/json",
-						}),
-					},
-				);
-			} catch (e) {
-				console.log(e);
-				return bad;
-			}
-		} catch (e) {
-			console.log(e);
-			await client.emit("interactionError", e);
-			return bad;
-		}
+	POST(ctx: HandlerContext): Promise<Response> {
+		return new Promise((res) =>
+			client.verifyFetchEvent({
+				respondWith: res,
+				request: ctx.req,
+			})
+		);
 	},
 };
