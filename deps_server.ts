88ce2b14a493d27a00cb058275aeaad62f5ccd57
Luca Matei Pintilie <lucafulger@gmail.com>
2022-07-02 23:58:01 +0200

    Update harmony discord lib

--- deps_server.ts
+++ deps_server.ts
@@ -10,12 +10,12 @@ export {
 	InteractionsClient,
 	slash,
 	SlashCommandOptionType,
-} from "https://code.harmony.rocks/08dc7b99089d94d62d6c87bf26e279746dfe418a/mod.ts";
+} from "https://code.harmony.rocks/3f591254deb572e3ffb4f2349d9a181ea49d3e33/mod.ts";
 export type {
 	ApplicationCommandChoice,
 	ApplicationCommandOption,
 	SlashCommandPartial,
-} from "https://code.harmony.rocks/08dc7b99089d94d62d6c87bf26e279746dfe418a/mod.ts";
+} from "https://code.harmony.rocks/3f591254deb572e3ffb4f2349d9a181ea49d3e33/mod.ts";
 
 export { config } from "https://deno.land/std@0.146.0/dotenv/mod.ts";
 
